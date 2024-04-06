170a86b96291e59895d773451051469096d5a78d
KosterSCA <100853317+KosterSCA@users.noreply.github.com>
2024-04-06 14:02:36 +0200

    Create 2_TreePL.sh

---
+++ Scripts/4_treePL/2_TreePL.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --partition=cpu_natbio
+#SBATCH --output=output_%j.txt
+#SBATCH --time=0-01:00:00
+#SBATCH --error=error_output_%j.txt
+#SBATCH --job-name=TreePL_Ommatotriton
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=stephaniekoster222@gmail.com
+
+cd /data1/s2321041/Ommatotriton/treePL/
+
+source /cm/shared/easybuild/software/AMUSE-Miniconda2/4.7.10/etc/profile.d/conda.sh
+conda activate /data1/s2321041/treepl
+
+/data1/s2321041/treepl/treePL 2_Omma_treePL.txt
